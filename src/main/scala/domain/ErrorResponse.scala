6375910cbfd990272e2d9d7ee62d413b8ca78824
Piotr Rudnicki <deamondev@gmail.com>
2022-08-07 11:23:16 +0200

    new enum

--- src/main/scala/domain/ErrorResponse.scala
+++ src/main/scala/domain/ErrorResponse.scala
@@ -1,8 +1,12 @@
 package domain
 
-enum ErrorResponse(msg: String): 
+enum ErrorResponse(msg: String):
   case EmptyLinkList extends ErrorResponse("No Links yet")
+  case AlreadyInUse(link: String)
+      extends ErrorResponse(s"The link $link is already in use.")
 
 object ErrorResponse:
-  given linkDecoder: zio.json.JsonDecoder[ErrorResponse] = zio.json.DeriveJsonDecoder.gen[ErrorResponse]
-  given linkEncoder: zio.json.JsonEncoder[ErrorResponse] = zio.json.DeriveJsonEncoder.gen[ErrorResponse]
+  given linkDecoder: zio.json.JsonDecoder[ErrorResponse] =
+    zio.json.DeriveJsonDecoder.gen[ErrorResponse]
+  given linkEncoder: zio.json.JsonEncoder[ErrorResponse] =
+    zio.json.DeriveJsonEncoder.gen[ErrorResponse]
